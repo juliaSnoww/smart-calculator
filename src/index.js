0539d6b466fc87a9d359684ce2255de41ea836d0
Julia Pritychenko <juliasnoww@mail.ru>
2018-10-06 18:09:31 +0300

    awesome math

--- src/index.js
+++ src/index.js
@@ -1,27 +1,76 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.initialValue = initialValue.toString();
+        return this;
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.initialValue += ('+' + number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.initialValue += ('-' + number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.initialValue += ('*' + number);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.initialValue += ('/' + number);
+        return this;
+    }
+
+    pow(number) {
+        this.initialValue += ('^' + number);
+        return this;
+    }
+
+    valueOf() {
+        let value = this.initialValue;
+        let result = 0;
+        value = value.match(/\+|\*|\^|\/|\d+|\-/g);
+
+        while (value.indexOf('^') !== -1) {
+            let index = value.lastIndexOf('^');
+            result = Math.pow(value[index - 1], value[index + 1]);
+            calculation(index);
+        }
+
+        while (value.indexOf('*') !== -1) {
+            let index = value.indexOf('*');
+            result = value[index - 1] * value[index + 1];
+            calculation(index);
+        }
+
+        while (value.indexOf('/') !== -1) {
+            let index = value.indexOf('/');
+            result = value[index - 1] / value[index + 1];
+            calculation(index);
+        }
+
+        while (value.indexOf('-') !== -1) {
+            let index = value.indexOf('-');
+            result = Number(value[index - 1]) - Number(value[index + 1]);
+            calculation(index);
+        }
+
+        while (value.indexOf('+') !== -1) {
+            let index = value.indexOf('+');
+            result = Number(value[index - 1]) + Number(value[index + 1]);
+            calculation(index);
+        }
+
+        function calculation(index) {
+            value[index - 1] = result;
+            value.splice(index, 2);
+        }
+
+        return result;
+    }
 }
 
 module.exports = SmartCalculator;
